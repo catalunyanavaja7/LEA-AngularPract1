b97fb7352de7c898d90bb7775a201717bd88d25e
yolo1 <leonardo.mata@institutvidreres.cat>
2025-12-11 12:22:29 +0100

    exacto, estoy en mi peak 2

--- src/app/llista-correus/llista-correus.ts
+++ src/app/llista-correus/llista-correus.ts
@@ -1,18 +1,24 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForOf} from '@angular/common';
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-llista-correus',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './llista-correus.html',
   styleUrl: './llista-correus.css',
   standalone: true
 })
 export class LlistaCorreus implements OnInit {
-
+  count = 0
+  count2 = 0
   correus = [
-    {numero: "Correo 1",emissor: "alexis@gmail.com", texto:"Hola buenas le informo que te calles", leido: false,},
-    {numero: "Correo 2",emissor: "gorka@gmail.com", texto:"Se a caducao tu suscripcion paga perro", leido: true,}
+    {numero: "Correo 1",emissor: "alexis@gmail.com", texto:"Hola buenas le informo que te calles", leido: false,contador: this.count},
+    {numero: "Correo 2",emissor: "aleix@gmail.com", texto:"Hola buenas le que te calles", leido: false,contador: this.count},
+    {numero: "Correo 3",emissor: "asuraxd@gmail.com", texto:"Hola le informo que te calles", leido: true,contador: this.count2},
+    {numero: "Correo 4",emissor: "gorka@gmail.com", texto:"Se a caducao tu suscripcion paga perro", leido: true,contador: this.count2},
+    {numero: "Correo 5",emissor: "timusin@gmail.com", texto:"Se a tu suscripcion paga perro", leido: true,contador: this.count2}
   ];
 
   ngOnInit(): void {
